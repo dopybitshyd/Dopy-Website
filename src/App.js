333f86131161f9a2d93d353bbc24ff928f6df381
Sameer-sus <f20212484@hyderabad.bits-pilani.ac.in>
2023-07-17 23:17:53 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+function App() {
+  return (
+    <div className="App-header">
+      <div className="App">
+        hello
+      </div>
+   
+    </div>
+  );
+}
+export default App;
