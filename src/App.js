e9b08af9fdfb129127888e2e7b9c0c80d556cb60
dopybitshyd <dopy@hyderabad.bits-pilani.ac.in>
2023-07-28 10:48:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Navbar from "./components/Navbar"
 function App() {
   return (
     <div className="App-header">
+
       <div className="App">
-        hello
+          <Navbar/>
       </div>
-   
     </div>
   );
 }
---
+++ src/components/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links",
+        icon: "fa-solid fa-camera"
+    },
+    {
+        title: "Photo Gallery",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Work Request",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Sign Up",
+        url: "#",
+        cName: "nav-links-mobile"
+    }
+]
